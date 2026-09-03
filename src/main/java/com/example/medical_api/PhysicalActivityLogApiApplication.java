7a837c8a12d6c6d9290189b309125219f940ae44
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2026-09-03 10:00:28 -0500

    feat(inicial): agregar estructura básica del proyecto con configuración de Maven y reglas de codificación

---
+++ src/main/java/com/example/medical_api/PhysicalActivityLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.medical_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalActivityLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalActivityLogApiApplication.class, args);
+	}
+
+}
